6c1f0b3e880c4fc5ca5d9aee1153595c25c7aa1a
Jpiemaz <jmazzoli@gmail.com>
2014-03-03 16:47:54 -0500

    object orientation

--- day5/hangman.rb
+++ day5/hangman.rb
@@ -1,59 +1,60 @@
-filename = ARGV.shift
-
-# if ARGV[0] == ""
-#   puts "You must enter a file"
-#   abort
-# end
-word_bank = []
-dictionary = File.open(filename, "r")
-dictionary.each do |words|
-  word_bank << words
-end
-
-word = word_bank.sample
-blanks = "_ " * (word.length - 1)
-word = (word.split("")).join(" ")
-
-puts "This is Hangman! Guess!"
-guess_number = 6
-puts "#{guess_number} guesses remaining"
-puts 
-puts blanks.strip
-print "> "
-guess = gets.strip
-
-
-guesses = []
-
-
-while blanks.strip != word.strip
-  guesses << guess
-  no = 0
-  0.upto(word.length) do |n|
-    if guess == word[n]
-      blanks[n] = guess
-    else
-      no = no + 1
+class Hangman
+  attr_accessor :blanks, :word, :guesses
+  
+  def initialize(filename)
+    dictionary = File.open(filename, "r")
+    word_bank = dictionary.to_a
+    word = word_bank.sample
+    @blanks = "_ " * (word.length - 1)
+    @word = (word.split("")).join(" ")
+    @guess_number = 6
+    @guesses = []
+  end
+  
+  def play
+    print_welcome_message
+    correct_guess?
+  end
+  
+  def correct_guess?
+    while blanks.strip != word.strip
+      guess = get_guess
+      guesses << guess
+      no = 0
+      0.upto(word.length) do |n|
+        if guess == word[n]
+          blanks[n] = guess
+        else
+          no = no + 1
+        end
+      end
+      if no == word.length + 1
+        @guess_number = @guess_number - 1
+      end
+      if @guess_number == 0
+        puts
+        puts "You lose, the word was:"
+        puts word.strip
+        abort
+      end
+      if blanks.strip == word.strip
+        puts blanks.strip
+        puts "You win"
+        abort
+      end
     end
   end
-  if no == word.length + 1
-    guess_number = guess_number - 1
-  end
-  if guess_number == 0
+  def get_guess
+    puts "#{@guess_number} guesses remaining"
     puts
-    puts "You lose, the word was:"
-    puts word.strip
-    abort
-  end
-  if blanks.strip == word.strip
+    puts (guesses.sort).join(" ")
+    puts 
     puts blanks.strip
-    puts "You win"
-    abort
+    print "> "
+    gets.strip
   end
-  puts "#{guess_number} guesses remaining"
-  puts (guesses.sort).join(" ")
-  puts blanks.strip
-  print "> "
-  guess = gets.strip
+  def print_welcome_message
+    puts "This is Hangman! Guess!"
+  end
 end
-
+  
---
+++ day5/main.rb
@@ -0,0 +1,61 @@
+require "./hangman"
+
+hangman = Hangman.new(ARGV.shift)
+hangman.play
+
+
+
+
+
+# filename = ARGV.shift
+# 
+# 
+# dictionary = File.open(filename, "r")
+# word_bank = dictionary.to_a
+# 
+# 
+# word = word_bank.sample
+# blanks = "_ " * (word.length - 1)
+# word = (word.split("")).join(" ")
+# 
+# puts "This is Hangman! Guess!"
+# guess_number = 6
+# puts "#{guess_number} guesses remaining"
+# puts 
+# puts blanks.strip
+# print "> "
+# guess = gets.strip
+# 
+# guesses = []
+# 
+# while blanks.strip != word.strip
+#   guesses << guess
+#   no = 0
+#   0.upto(word.length) do |n|
+#     if guess == word[n]
+#       blanks[n] = guess
+#     else
+#       no = no + 1
+#     end
+#   end
+#   if no == word.length + 1
+#     guess_number = guess_number - 1
+#   end
+#   if guess_number == 0
+#     puts
+#     puts "You lose, the word was:"
+#     puts word.strip
+#     abort
+#   end
+#   if blanks.strip == word.strip
+#     puts blanks.strip
+#     puts "You win"
+#     abort
+#   end
+#   puts "#{guess_number} guesses remaining"
+#   puts (guesses.sort).join(" ")
+#   puts blanks.strip
+#   print "> "
+#   guess = gets.strip
+# end
+# 
